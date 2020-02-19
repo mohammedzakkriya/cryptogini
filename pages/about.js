bd260f9ce889adfb4d32a1a0b88ea36c6debf0b8
Apple <apple@apples-macbook-pro.local>
2020-02-18 21:50:13 -0500

    some css changes

--- pages/about.js
+++ pages/about.js
@@ -5,7 +5,7 @@ import Footer from '../components/footer'
 const About = () => (
     <div className="container-fluid d-flex h-100 mx-auto flex-column">
         <Header />
-        <main role="main" className="inner cover">
+        <main role="main" className="main-content">
             <h1 className="cover-heading">About page</h1>
             <p className="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         
